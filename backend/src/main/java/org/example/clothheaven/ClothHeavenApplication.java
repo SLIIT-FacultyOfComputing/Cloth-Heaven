2a46757d77e5cc8597caf66016fe1a0555cc6311
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-08-20 01:24:37 +0530

    initial commit

---
+++ backend/src/main/java/org/example/clothheaven/ClothHeavenApplication.java
@@ -0,0 +1,13 @@
+package org.example.clothheaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothHeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothHeavenApplication.class, args);
+	}
+
+}
